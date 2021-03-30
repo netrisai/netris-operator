4ebe15c330ee68877881aa52e8c7e075dbd51f32
Artashes Balabekyan <artashes.balabekyan@netris.ai>
2021-03-30 18:19:40 +0400

    fix: l4lb logger default

--- main.go
+++ main.go
@@ -109,7 +109,7 @@ func main() {
 	}
 	if err = (&controllers.L4LBReconciler{
 		Client: mgr.GetClient(),
-		Log:    ctrl.Log.WithName("controllers").WithName("L4LB"),
+		Log:    ctrl.Log.WithName("L4LB"),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "L4LB")
