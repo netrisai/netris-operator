997199c3e9a4d1435862487b85b605d7778e09b1
Artashes Balabekyan <artashes.balabekyan@netris.ai>
2022-02-06 22:24:15 +0400

    fix: softgate: auto ip handle process

--- controllers/softgatemeta_controller.go
+++ controllers/softgatemeta_controller.go
@@ -138,9 +138,6 @@ func (r *SoftgateMetaReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error
 		if apiSoftgate, ok := r.NStorage.HWsStorage.FindSoftgateByID(softgateMeta.Spec.ID); ok {
 			debugLogger.Info("Comparing SoftgateMeta with Netris Softgate")
 
-			softgateMeta.Spec.MainIP = apiSoftgate.MainIP.Address
-			softgateMeta.Spec.MgmtIP = apiSoftgate.MgmtIP.Address
-
 			if ok := compareSoftgateMetaAPIESoftgate(softgateMeta, apiSoftgate, u); ok {
 				debugLogger.Info("Nothing Changed")
 			} else {
@@ -162,6 +159,8 @@ func (r *SoftgateMetaReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error
 				}
 				logger.Info("Softgate Updated")
 			}
+			softgateMeta.Spec.MainIP = apiSoftgate.MainIP.Address
+			softgateMeta.Spec.MgmtIP = apiSoftgate.MgmtIP.Address
 		} else {
 			debugLogger.Info("Softgate not found in Netris")
 			debugLogger.Info("Going to create Softgate")
