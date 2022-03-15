666c80fd7a24d8e49c159d5fa6d643c30cf369f1
Artashes Balabekyan <balabekyanartashes@gmail.com>
2022-03-15 11:49:28 +0400

    fix: nat: action field validation

--- api/v1alpha1/nat_types.go
+++ api/v1alpha1/nat_types.go
@@ -34,7 +34,7 @@ type NatSpec struct {
 	State string `json:"state,omitempty"`
 	Site  string `json:"site"`
 
-	// +kubebuilder:validation:Enum=dnat;snat;acept_snat;masquarade
+	// +kubebuilder:validation:Enum=dnat;snat;accept_snat;masquarade
 	Action string `json:"action"`
 
 	// +kubebuilder:validation:Enum=all;tcp;udp;icmp
